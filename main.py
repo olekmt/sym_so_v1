2c486201571aa1ef3bfaec2138e7c515dc92f98b
olekmt <aleksandermatlok@gmail.com>
2020-01-12 22:19:04 +0100

    bug fix

--- main.py
+++ main.py
@@ -34,56 +34,51 @@ def input_processes(n, p):
     return processes
 
 
-con = 1
-while con == 1:
-    b = input("Choose an algorithm"
-              "\n1 - FCFS"
-              "\n2 - SJF"
-              "\n3 - Round-robin"
-              "\n4 - Priority Based"
-              "\nelse - exit"
-              "\n")
+b = input("Choose an algorithm"
+          "\n1 - FCFS"
+          "\n2 - SJF"
+          "\n3 - Round-robin"
+          "\n4 - Priority Based"
+          "\nelse - exit"
+          "\n")
 
-    if b != "1" and b != "2" and b != "3" and b != "4":
-        exit(0)
+if b != "1" and b != "2" and b != "3" and b != "4":
+    exit(0)
 
-    a = input("1 - read processes from file"
-              "\n2 - get processes from user"
-              "\nelse - exit"
-              "\n")
+a = input("1 - read processes from file"
+          "\n2 - get processes from user"
+          "\nelse - exit"
+          "\n")
 
-    if a == "2":
+if a == "2":
+    n = return_n()
+    while n == 0:
+        print("Error, n=0. Choose n greater than 0")
         n = return_n()
-        while n == 0:
-            print("Error, n=0. Choose n greater than 0")
-            n = return_n()
-        if b == "1" or b == "3" or b == "2":
-            processes = input_processes(n, 0)
-        elif b == "4":
-            processes = input_processes(n, 1)
-    elif a == "1":
-        p1 = read()
-        data = p1[0]
-        processes = p1
-        del processes[0]
-        n = data[0]
-    else:
-        exit(0)
-
-    if b == "1":
-        fcfs = FCFS(processes, n)
-        fcfs.find_avg(processes, n)
-    elif b == "2":
-        sjf = SJF(processes, n)
-        sjf.find_avg(processes, n)
-    elif b == "3":
-        rr = RR(processes, n)
-        rr.find_avg(processes, n)
+    if b == "1" or b == "3" or b == "2":
+        processes = input_processes(n, 0)
     elif b == "4":
-        pbs = PBS(processes, n)
-        pbs.find_avg(processes, n)
-    else:
-        exit(0)
+        processes = input_processes(n, 1)
+elif a == "1":
+    p1 = read()
+    data = p1[0]
+    processes = p1
+    del processes[0]
+    n = data[0]
+else:
+    exit(0)
 
-    con = input("continue? 1 for yes, 2 for no: ")
-    con = int(con)
+if b == "1":
+    fcfs = FCFS(processes, n)
+    fcfs.find_avg(processes, n)
+elif b == "2":
+    sjf = SJF(processes, n)
+    sjf.find_avg(processes, n)
+elif b == "3":
+    rr = RR(processes, n)
+    rr.find_avg(processes, n)
+elif b == "4":
+    pbs = PBS(processes, n)
+    pbs.find_avg(processes, n)
+else:
+    exit(0)
