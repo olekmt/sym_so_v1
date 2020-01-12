fc25b4661c4c65f600211a2947bbccb3c350c5a0
olekmt <aleksandermatlok@gmail.com>
2020-01-12 22:35:36 +0100

    bug fix

--- main.py
+++ main.py
@@ -4,9 +4,6 @@ from classes.PBS import *
 from classes.RR import *
 from classes.input import *
 
-processes = []
-n = 0
-
 
 def return_n():
     nn = input("Number of processes: ")
@@ -34,51 +31,63 @@ def input_processes(n, p):
     return processes
 
 
-b = input("Choose an algorithm"
-          "\n1 - FCFS"
-          "\n2 - SJF"
-          "\n3 - Round-robin"
-          "\n4 - Priority Based"
-          "\nelse - exit"
-          "\n")
+con = "1"
 
-if b != "1" and b != "2" and b != "3" and b != "4":
-    exit(0)
+while con != "2":
+    processes = []
+    n = 0
 
-a = input("1 - read processes from file"
-          "\n2 - get processes from user"
-          "\nelse - exit"
-          "\n")
+    b = input("Choose an algorithm"
+              "\n1 - FCFS"
+              "\n2 - SJF"
+              "\n3 - Round-robin"
+              "\n4 - Priority Based"
+              "\nelse - exit"
+              "\n")
 
-if a == "2":
-    n = return_n()
-    while n == 0:
-        print("Error, n=0. Choose n greater than 0")
+    if b != "1" and b != "2" and b != "3" and b != "4":
+        exit(0)
+
+    a = input("1 - read processes from file"
+              "\n2 - get processes from user"
+              "\nelse - exit"
+              "\n")
+
+    if a == "2":
         n = return_n()
-    if b == "1" or b == "3" or b == "2":
-        processes = input_processes(n, 0)
-    elif b == "4":
-        processes = input_processes(n, 1)
-elif a == "1":
-    p1 = read()
-    data = p1[0]
-    processes = p1
-    del processes[0]
-    n = data[0]
-else:
-    exit(0)
+        while n == 0:
+            print("Error, n=0. Choose n greater than 0")
+            n = return_n()
+        if b == "1" or b == "3" or b == "2":
+            processes = input_processes(n, 0)
+        elif b == "4":
+            processes = input_processes(n, 1)
+    elif a == "1":
+        p1 = read()
+        data = p1[0]
+        processes = p1
+        del processes[0]
+        n = data[0]
+    else:
+        exit(0)
 
-if b == "1":
-    fcfs = FCFS(processes, n)
-    fcfs.find_avg(processes, n)
-elif b == "2":
-    sjf = SJF(processes, n)
-    sjf.find_avg(processes, n)
-elif b == "3":
-    rr = RR(processes, n)
-    rr.find_avg(processes, n)
-elif b == "4":
-    pbs = PBS(processes, n)
-    pbs.find_avg(processes, n)
-else:
-    exit(0)
+    if b == "1":
+        fcfs = FCFS(processes, n)
+        fcfs.find_avg(processes, n)
+        del fcfs
+    elif b == "2":
+        sjf = SJF(processes, n)
+        sjf.find_avg(processes, n)
+        del sjf
+    elif b == "3":
+        rr = RR(processes, n)
+        rr.find_avg(processes, n)
+        del rr
+    elif b == "4":
+        pbs = PBS(processes, n)
+        pbs.find_avg(processes, n)
+        del pbs
+    else:
+        exit(0)
+
+    con = input("continue? 1 for yes, 2 for no: ")
